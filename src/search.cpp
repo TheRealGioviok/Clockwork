99cd28dc25d9d62feced9dc9ba624a692967a7d2
TheRealGioviok <425gioviok@gmail.com>
2025-09-14 20:08:45 +0200

    Bench: 2586229

--- src/search.cpp
+++ src/search.cpp
@@ -415,7 +415,7 @@ Value Worker::search(
     }
 
     // Razoring
-    if (!PV_NODE && !is_in_check && depth <= 7 && ss->static_eval + 707 * depth < alpha) {
+    if (!PV_NODE && !is_in_check && depth <= 7 && tt_adjusted_eval + 707 * depth < alpha) {
         const Value razor_score = quiesce<IS_MAIN>(pos, ss, alpha, beta, ply);
         if (razor_score <= alpha) {
             return razor_score;
