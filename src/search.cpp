b4f92b44b393e834fcc28d58673741b7c9bd3b18
TheRealGioviok <425gioviok@gmail.com>
2025-10-16 14:21:41 +0200

    Bench: 13358390

--- src/search.cpp
+++ src/search.cpp
@@ -556,6 +556,9 @@ Value Worker::search(
             else if (tt_data->score >= beta) {
                 extension = -1 - PV_NODE;
             }
+            else if (cutnode) {
+                extension = -1;
+            }
         }
 
         // Do move
