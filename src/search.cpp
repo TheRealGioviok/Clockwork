65a240bc5d7ab7503fbd3db681f6daa18e4a1b65
TheRealGioviok <425gioviok@gmail.com>
2025-11-05 05:09:30 +0100

    Bench: 11412486

--- src/search.cpp
+++ src/search.cpp
@@ -519,7 +519,8 @@ Value Worker::search(
         const auto nodes_before = m_search_nodes.load(std::memory_order::relaxed);
         bool       quiet        = quiet_move(m);
 
-        auto move_history = quiet ? m_td.history.get_quiet_stats(pos, m, ply, ss) : 0;
+        auto move_history = quiet ? m_td.history.get_quiet_stats(pos, m, ply, ss)
+                                  : m_td.history.get_noisy_stats(pos, m);
 
         if (!ROOT_NODE && !is_being_mated_score(best_value)) {
             // Late Move Pruning (LMP)
@@ -674,6 +675,8 @@ Value Worker::search(
 
             if (!quiet) {
                 reduction = std::min(reduction, 3072);
+                reduction +=
+                  (512 - move_history / 16);  // Leaving duplicate here for tunability later
             }
 
             reduction /= 1024;
