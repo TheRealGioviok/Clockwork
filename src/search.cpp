641f610a560876deca1743951c616e7c537a9115
TheRealGioviok <425gioviok@gmail.com>
2025-11-07 16:38:47 +0100

    Bench: 12753163

--- src/search.cpp
+++ src/search.cpp
@@ -450,7 +450,7 @@ Value Worker::search(
     }
 
     if (!PV_NODE && !is_in_check && depth <= tuned::rfp_depth && !excluded
-        && tt_adjusted_eval >= beta + tuned::rfp_margin * depth) {
+        && tt_adjusted_eval >= beta + tuned::rfp_margin * depth && (!tt_data || tt_data->bound() != Bound::Lower)) {
         return tt_adjusted_eval;
     }
 
