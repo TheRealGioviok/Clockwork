1655185381cc6ee0f5f4baac87ca2d9cbfc603c7
TheRealGioviok <425gioviok@gmail.com>
2026-04-24 04:00:16 +0200

    Bench: 17132227

--- src/evaluation.cpp
+++ src/evaluation.cpp
@@ -259,7 +259,8 @@ PScore evaluate_pieces(const Position& pos) {
                                          ? Bitboard::rank_mask(1) | Bitboard::rank_mask(2)
                                          : Bitboard::rank_mask(5) | Bitboard::rank_mask(6);
     Bitboard           own_early_pawns = own_pawns & early_ranks;
-    Bitboard bb  = (blocked_pawns | own_early_pawns) | pos.attacked_by(opp, PieceType::Pawn) | pos.bitboard_for(color, PieceType::King);
+    Bitboard bb = (blocked_pawns | own_early_pawns) | pos.attacked_by(opp, PieceType::Pawn)
+                | pos.bitboard_for(color, PieceType::King);
     Bitboard bb2 = bb;
 
     for (PieceId id : pos.get_piece_mask(color, PieceType::Knight)) {
