833ebbf2e5580bc0134deb29877b01864dabb94c
TheRealGioviok <425gioviok@gmail.com>
2026-04-27 13:48:49 +0200

    Bench: 14931024

--- src/search.cpp
+++ src/search.cpp
@@ -488,7 +488,7 @@ Value Worker::search(
         tt_adjusted_eval = tt_data->score;
     }
 
-    if (!PV_NODE && !is_in_check && depth <= tuned::rfp_depth && !excluded
+    if (!PV_NODE && !ttpv && !is_in_check && depth <= tuned::rfp_depth && !excluded
         && tt_adjusted_eval >= beta + tuned::rfp_margin * depth) {
         return tt_adjusted_eval;
     }
