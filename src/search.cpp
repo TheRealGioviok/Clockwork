22849e1ef414095917f2695b3dfaf16451ca669d
TheRealGioviok <425gioviok@gmail.com>
2026-09-21 08:40:01 +0200

    Bench: 12668862

--- src/search.cpp
+++ src/search.cpp
@@ -891,6 +891,8 @@ Value Worker::search(
                                                  * log2i(moves_played) / (1024 * 1024));
             }
 
+            reduction -= 3072 * (abs(correction) - 80) / 1024;
+
             reduction -= tuned::lmr_pv_node_red * PV_NODE;
 
             reduction += alpha_raises * tuned::lmr_alpha_raise_red;
